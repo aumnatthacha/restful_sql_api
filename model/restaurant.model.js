061a2778d5478edd97911b3904ae361a8ff558f7
aumnatthacha <aummumdaeng@gmail.com>
2023-07-19 11:38:11 +0700

    3

--- model/restaurant.model.js
+++ model/restaurant.model.js
@@ -1,1 +1,34 @@
-const {DataType} = require("sequelize");
+const { DataType, DataTypes } = require("sequelize");
+const sequelize = require("./db");
+//Define the restaurant model
+const Restaurant = sequelize.define("re", {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    type: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    img: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        defaultValue: DataTypes.NOW,
+    },
+});
+
+module.exports = Restaurant;
